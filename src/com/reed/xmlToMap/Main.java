953514281bb0d90c17e3b22799d6b2e2ef22c3e9
reed <769705252@qq.com>
2020-01-13 09:27:41 +0800

    xml转map完结篇

--- src/com/reed/xmlToMap/Main.java
+++ src/com/reed/xmlToMap/Main.java
@@ -7,10 +7,12 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Map map = Utils.xmlToMap("C:\\Users\\luwei\\IdeaProjects\\Note\\src\\com\\reed\\xmlToMap\\test.xml");
-        System.out.println(map);
-//        BufferedReader xmlReader = XmlToMapUtils.getXmlReader("C:\\Users\\luwei\\IdeaProjects\\Note\\src\\com\\reed\\xmlToMap\\test.xml");
-//        Map praser = XmlToMapUtils.praser(xmlReader, new HashMap(),-2);
-//        System.out.println(praser);
+//        Map map = Utils.xmlToMap("C:\\Users\\luwei\\IdeaProjects\\Note\\src\\com\\reed\\xmlToMap\\test.xml");
+//        System.out.println(map);
+        BufferedReader xmlReader = XmlToMapUtils.getXmlReader("C:\\Users\\Elead\\IdeaProjects\\Note\\src\\com\\reed\\xmlToMap\\test.xml");
+        Map praser = XmlToMapUtils.praser(xmlReader, new HashMap(),-2);
+        xmlReader.close();
+        System.out.println(praser);
+
     }
 }
--- src/com/reed/xmlToMap/XmlToMapUtils.java
+++ src/com/reed/xmlToMap/XmlToMapUtils.java
@@ -23,15 +23,18 @@ public class XmlToMapUtils {
             temp = (char)read;
             //跳过标签间隔空白，换行，回车
             if(read=='\r'||read=='\n'||read==' ')
-                temp = skipEmpty(reader);
+                temp = (char)skipEmpty(reader);
             if(temp=='<'){
                 String key = getStringFromIndexToChar(reader,'>');
+                //返回上一层
+                if(key.startsWith("/"))return map;
+                //检查是否为正确的key
                 if(checkIsKey(key)) {
-                    temp =skipEmpty(reader);
+                    temp =(char)skipEmpty(reader);
                     if(temp!='<'){
                         String value = getStringFromIndexToChar(reader,'<');
-                        map.put(key,value);
-
+                        map.put(key,temp+value);
+                        skipToGivenChar(reader,'>');
                     }else{
                         HashMap<String,Object> next = new HashMap<>();
                         map.put(key,next);
@@ -39,35 +42,42 @@ public class XmlToMapUtils {
                     }
                 }
             }
-            read = reader.read();
+            read = skipEmpty(reader);
         }
         return map;
     }
-
-    private static boolean checkIsKey(String string) {
-        return true;
+    //跳到指定的字符
+    private static void skipToGivenChar(BufferedReader reader,char c) throws IOException {
+        int temp = reader.read();
+        while(temp!=-1&&(char)temp!=c){
+            temp = reader.read();
+        }
     }
 
-
-
+    //检查获取的key是否符合标准
+    private static boolean checkIsKey(String string) {
+        if(string.startsWith("?")&&string.endsWith("?"))return false;
+        if(string.endsWith("/"))return false;
+        if(string.startsWith("!--")&&string.endsWith("--"))return false;
+        return true;
+    }
     //获取到下一个给出字符的字符串
     private static String getStringFromIndexToChar(BufferedReader reader, char c) throws IOException {
         int read = reader.read();
         StringBuilder string = new StringBuilder();
         while (read!=-1&&(char)read!=c){
+            if((char)read!='\r'&&(char)read!='\n')
             string.append((char)read);
             read = reader.read();
         }
         return string.toString();
     }
-
-    public static char skipEmpty(BufferedReader reader) throws IOException {
-        char read = (char)reader.read();
-        while(read=='\t'||read=='\n'||read==' ') {
+    //跳过空白换行回车
+    public static int skipEmpty(BufferedReader reader) throws IOException {
+        int read = reader.read();
+        while((read=='\r'||read=='\n'||read==' ')&&read!=-1) {
             read = (char) reader.read();
         }
         return read;
     }
-
-
 }
